04ef7bdb4727f61e1dd58513e15496062a4246f0
Michael Barbini <michaelbarbini@gmail.com>
2016-07-05 14:19:37 -0400

    This allows for images to be read from a URL, it reads different images based off of the URL changing and will need to implement a button click to facilitate this

--- mechanical_turk/src/org/neu/ccs/mechanical_turk/UploadImage.java
+++ mechanical_turk/src/org/neu/ccs/mechanical_turk/UploadImage.java
@@ -1,5 +1,38 @@
 package org.neu.ccs.mechanical_turk;
 
-public class UploadImage {
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+
+public class UploadImage 
+{
+	String imageURL = null;
+	URL url = null;
+	File file = null;
+	public void importImage() 
+	{
+		String serverURL = "";
+		for(int i = 0; i <1000; i+=1)
+		{
+			imageURL = serverURL + Integer.toString(i);
+			System.out.println(imageURL);
+		}
+	}
+	
+	public void readImage()
+	{
+		BufferedImage img = null;
+		file = new File(imageURL);
+		try 
+		{
+			img = ImageIO.read(file);  
+		} 
+		catch (IOException e) 
+		{
+		}
+	}
 }
