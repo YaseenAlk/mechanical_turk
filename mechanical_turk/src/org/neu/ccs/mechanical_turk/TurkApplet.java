8147c9c1896e4e08e80736ff9d453f545a31e8c0
Yaseen Alkhafaji <alkhafaji.yaseen@gmail.com>
2016-07-05 11:30:30 -0400

    Add TurkApplet class

---
+++ mechanical_turk/src/org/neu/ccs/mechanical_turk/TurkApplet.java
@@ -0,0 +1,16 @@
+package org.neu.ccs.mechanical_turk;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class TurkApplet extends Applet {
+	
+	public void init() {
+		//will we need to receive any parameters?
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+
+}
